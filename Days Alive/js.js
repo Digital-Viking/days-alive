905c888bbbaa784519de7aff43b6e296a6e5aa56
David Weston Webb <westwebb@gmail.com>
2019-02-21 14:56:21 -0500

    Add files via upload

---
+++ Days Alive/js.js
@@ -0,0 +1,9 @@
+var VisitorsAge = prompt("How old are you?");
+
+var YearsAlive = parseInt(VisitorsAge);
+
+    YearsAlive = (YearsAlive - 1) * 365;
+
+document.write("You have been alive for at least " + YearsAlive + " days! Great job and keep up the good work!");
+
+console.log("Program Complete - have a nice day");
